98b9d069388ff2b34598a27605c0bdffc70a6353
Morteza Farrokhnejad <152209810+IAmFarrokhnejad@users.noreply.github.com>
2024-05-17 06:01:07 +0300

    Add files via upload

---
+++ src/coffeeshopmanagementsystem/Customer.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package coffeeshopmanagementsystempartone;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author afr51
+ */
+class Customer implements Serializable{
+    private int customer_id;
+    private String name;
+    private String surname;
+    private String email;
+    private String phone;
+
+    // Constructor
+    public Customer(int customer_id, String name, String surname, String email, String phone) {
+        this.customer_id = customer_id;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public int getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(int customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer [customer_id=" + customer_id + ", name=" + name + ", surname=" + surname + ", email=" + email
+                + ", phone=" + phone + "]";
+    }
+}
---
+++ src/coffeeshopmanagementsystem/Order.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package coffeeshopmanagementsystempartone;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author afr51
+ */
+class Order implements Serializable{
+    private int order_id;
+    private int emp_id;
+    private Date order_date;
+    private float total_amount;
+
+    // Constructor
+    public Order(int order_id, int emp_id, Date order_date, float total_amount) {
+        this.order_id = order_id;
+        this.emp_id = emp_id;
+        this.order_date = order_date;
+        this.total_amount = total_amount;
+    }
+
+    public int getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(int order_id) {
+        this.order_id = order_id;
+    }
+
+    public int getEmp_id() {
+        return emp_id;
+    }
+
+    public void setEmp_id(int emp_id) {
+        this.emp_id = emp_id;
+    }
+
+    public Date getOrder_date() {
+        return order_date;
+    }
+
+    public void setOrder_date(Date order_date) {
+        this.order_date = order_date;
+    }
+
+    public float getTotal_amount() {
+        return total_amount;
+    }
+
+    public void setTotal_amount(float total_amount) {
+        this.total_amount = total_amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Order [order_id=" + order_id + ", emp_id=" + emp_id + ", order_date=" + order_date + ", total_amount="
+                + total_amount + "]";
+    }
+
+    
+}
