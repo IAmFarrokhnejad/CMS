caeeca73e0f1a6a47a2462c4982dd947473f083c
Morteza Farrokhnejad <152209810+IAmFarrokhnejad@users.noreply.github.com>
2024-05-21 00:06:36 +0300

    Added GUI

---
+++ src/coffeeshopmanagementsystemparttwo/Product.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package coffeeshopmanagementsystemparttwo;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author MortezaFarrokhnejad
+ */
+class Product implements Serializable {
+    private int product_id;
+    private String name;
+    private float price;
+    private String category;
+    private int stock;
+
+    // Constructor
+    public Product(int product_id, String name, float price, String category, int stock) {
+        this.product_id = product_id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.stock = stock;
+    }
+
+    public int getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Product [product_id=" + product_id + ", name=" + name + ", price=" + price + ", category=" + category
+                + ", stock=" + stock + "]";
+    }
+
+}
